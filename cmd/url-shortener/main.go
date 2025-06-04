83e1eb0c65ae1ce56b61fc099f8bf8284f61bbc7
akhilsomanvs <akhilsomanvs@gmail.com>
2025-06-04 10:20:36 +0530

    Config file setup

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,3 +1,12 @@
-package urlshortener
+package main
 
-func main() {}
+import "github.com/akhilsomanvs/url-shortener/internal/config"
+
+func main() {
+	//load config
+	_ = config.MustLoad()
+
+	//database setuip
+	//setup router
+	//setup server
+}
